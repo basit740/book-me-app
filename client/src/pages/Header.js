3e4b309a04b9e3cc7ceaa069beb7293266070965
Yohanesk <y.koeswono2311@gmail.com>
2024-05-16 15:28:11 -0700

    Fix Title position

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,6 +1,5 @@
 import { Button, Card, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CountDown from "../components/functions/CountDown";
 import { motion } from "framer-motion";
 import AnimationTitles from "../components/functions/AnimationTitles";
 import { useNavigate } from "react-router-dom";
@@ -16,12 +15,11 @@ function Loading() {
 
   return (
     <div className="loading position-relative">
-      <Container className="d-flex justify-content-between align-items-center gap-md-5 flex-column flex-md-row mt-3 mt-xl-4 overflow-hidden">
+      <Container className="d-flex justify-content-between align-items-start gap-md-5 flex-column flex-md-row mt-3 mt-xl-4 overflow-hidden">
         <motion.div
           initial={{ x: -400 }}
           animate={{ x: 0 }}
           transition={{ duration: 0.8 }}
-          style={{ top: 0 }}
         >
           <AnimationTitles title="BARITO MiniSoccer" />
           <p className="gray-90 mt-3 fs-5">
@@ -33,23 +31,6 @@ function Loading() {
           >
             Book Now
           </Button>
-          {/* <div
-            style={{ color: "white" }}
-            className="d-none d-md-flex justify-content-between align-items-center my-4"
-          >
-            <div>
-              <h5 className="fw-bold fs-1">12K+</h5>
-              <span className="gray-100">properties</span>
-            </div>
-            <div>
-              <h5 className="fw-bold fs-1">10K+</h5>
-              <span className="gray-100">auction</span>
-            </div>
-            <div>
-              <h5 className="fw-bold fs-1">12K+</h5>
-              <span className="gray-100">developers</span>
-            </div>
-          </div> */}
         </motion.div>
         <motion.div
           initial={{ x: 400 }}
@@ -70,26 +51,6 @@ function Loading() {
               </div>
             </Card.Body>
           </Card>
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 0.8 }}
-          style={{ color: "white" }}
-          className="d-md-none d-flex justify-content-between align-items-center my-4 features"
-        >
-          <div>
-            <h5 className="fw-bold fs-1">12K+</h5>
-            <span className="gray-100">properties</span>
-          </div>
-          <div>
-            <h5 className="fw-bold fs-1">10K+</h5>
-            <span className="gray-100">auction</span>
-          </div>
-          <div>
-            <h5 className="fw-bold fs-1">12K+</h5>
-            <span className="gray-100">developers</span>
           </div>
         </motion.div>
       </Container>
