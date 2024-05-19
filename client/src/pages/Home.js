b66b05de503a264d9f3e7a3dd3560b27af4a8fd8
Yohanesk <y.koeswono2311@gmail.com>
2024-05-18 17:06:38 -0700

    Cleaning

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,14 +3,12 @@ import React from "react";
 import AboutUs from "./AboutUs";
 import Join from "./Join";
 import Loading from "./Header";
-import Partners from "./Partners";
 import Subscribe from "./Subscribe";
 
 export const Home = () => {
   return (
     <>
       <Loading />
-      {/* <Partners /> */}
       <AboutUs />
       <Join />
       <Subscribe />
