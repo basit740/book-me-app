5a3e32a86e1915f952581e306632c42cc0b6d179
Yohanesk <y.koeswono2311@gmail.com>
2024-05-25 21:08:19 -0700

    fix description color

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -89,7 +89,7 @@ export const Admin = () => {
     <Container className="my-4">
       <div className="calendar-container py-3">
         <Container>
-          <h5 className="text-white my-5 text-left">
+          <h5 className="text-white my-5 text-left" style={{ textAlign: 'left', alignSelf: 'flex-start' }}>
             Menyetujui Pesanan <br />
             1. Klik slot time yang berwarna <span style={{ fontWeight: 'bold', textDecoration: 'underline' , color: 'yellow'}}>kuning</span>  <br />
             2. Klik tombol "Approve" <br/>
